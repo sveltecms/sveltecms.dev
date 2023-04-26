358c9413b812874329724aafb634b636942b38b3
Anthony <anthonytoribio809@gmail.com>
2023-04-26 19:39:53 -0400

    updated svelteCMS

--- src/cms/funcs/uploadFile.ts
+++ src/cms/funcs/uploadFile.ts
@@ -2,26 +2,31 @@ import Utils from "cms/utils/server"
 import db from "cms/lib/db.server"
 import path from "path"
 import fs from "fs"
+import { CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY } from "$env/static/private"
 import type { RequestEvent,UploadFileFunc } from "."
 import type { AssetLoad } from "cms/types"
 
+// <form action="https://api.cloudinary.com/v1_1/{your-cloud-name}/image/upload" method="post" enctype="multipart/form-data">
+//   <input type="file" name="file" />
+//   <input type="text" name="upload_preset" value="{your-upload-preset}" />
+//   <input type="text" name="api_key" value="{your-api-key}" />
+//   <input type="submit" value="Upload" />
+// </form>
+
+const apiUrl = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`
 // TODO: handle errors
 export default async function handleFunc(event:RequestEvent,funcInputData:any,json:Function) {
     const formData = await event.request.formData()
     const file = formData.get('image') as File
     const { ext:fileExtension,name:fileName } = path.parse(file.name)
-    const fileBuffer = Buffer.from(await file.arrayBuffer())
-    const newFileName = Date.now()+fileExtension
-    fs.writeFileSync(newFileName,fileBuffer)
-    const res = await Utils.Cloudinary.uploadFile(newFileName)
-    fs.rmSync(newFileName)
+    const cloudinaryResp = await Utils.Cloudinary.uploadFormFile(file)
     const newAssetData:AssetLoad = {
-        publicID: res.public_id,
-        assetID: res.asset_id,
-        src: res.secure_url,
+        publicID: cloudinaryResp.public_id,
+        assetID: cloudinaryResp.asset_id,
+        src: cloudinaryResp.secure_url,
         title: fileName,
-        type: res.resource_type,
-        extension: res.format
+        type: cloudinaryResp.resource_type,
+        extension: cloudinaryResp.format
     }
     const insertedAssetRes = await db.collection("_assets").insertOne(newAssetData)
     const response:UploadFileFunc['output'] = {
